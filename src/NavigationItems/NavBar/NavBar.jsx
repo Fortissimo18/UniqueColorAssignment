5790d32474f2e2a0141fc123dc7274d0bef7783b
Natalie18 <lantingd@student.unimelb.edu.au>
2021-10-01 16:29:36 +1000

    Changed NavBar font style for better UX

--- src/NavigationItems/NavBar/NavBar.jsx
+++ src/NavigationItems/NavBar/NavBar.jsx
@@ -3,12 +3,12 @@ import { Menu } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
 const menuStyle = {
-   fontSize: '3vmin', 
-   fontWeight:'600'
+  fontSize: '3vmin',
+  fontWeight: '600'
 }
 
 const NavBar = () => (
-   <Menu secondary pointing floated='right' style={menuStyle}> 
+  <Menu secondary pointing floated='right' style={menuStyle}>
     <Menu.Item name='Version 1' as={NavLink} to='/' exact />
     <Menu.Item name='Verison 2' as={NavLink} to='/version2' />
     <Menu.Item name='Version 3' as={NavLink} to='/version3' />
